bf6784f1d30f6242530cf9e127a0da5c7bb3a537
haider <haider.shah@hotmail.com>
2019-09-05 11:56:05 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,27 @@
 def addition(num1, num2)
-
+return num1+num2
 end
 
 def subtraction(num1, num2)
-
+return num1-num2
 end
 
 def division(num1, num2)
-
+return num1/num2
 end
 
 def multiplication(num1, num2)
-
+return num1*num2
 end
 
 def modulo(num1, num2)
-
+return num1%num2
 end
 
 def square_root(num)
-
+  $x
+$x**2=num
+puts x
+return x
 end
+square_root(9)
